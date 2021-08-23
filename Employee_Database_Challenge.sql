2f544a132542422d71cc13823d56d7cdc1e0ae79
LukeMotley <85656361+LukeMotley@users.noreply.github.com>
2021-08-22 23:28:35 -0400

    Add files via upload

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,49 @@
+-- Deliverable 1: The Number of Retiring Employees by Title
+
+--1. Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+SELECT  e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO retirement_titles
+FROM employees as e
+FULL OUTER JOIN  titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, title DESC;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire.
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibity
+FROM employees as e
+LEFT OUTER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+LEFT OUTER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
